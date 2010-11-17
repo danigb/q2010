ad1b12ecf6e8b04b66f36d7b110c3aeff0591b42
danigb <danigb@gmail.com>
2010-11-17 14:45:13 +0100

    Editable surveys by admin

--- app/controllers/admin_surveys_controller.rb
+++ app/controllers/admin_surveys_controller.rb
@@ -4,6 +4,28 @@ class AdminSurveysController < AdminController
     @users = User.all
   end
 
+  def edit
+    raise ActiveRecord::RecordNotFound.new if params[:survey_name].blank?
+    @name = params[:survey_name]
+    @user = User.find params[:id]
+    @survey = @user.send("survey_#{@name}")
+  end
+
+  def update
+    raise ActiveRecord::RecordNotFound.new if params[:survey_name].blank?
+    @name = params[:survey_name]
+    @user = User.find params[:id]
+    @survey = @user.send("survey_#{@name}")
+    data = params["survey_#{@name}".to_sym]
+    if @survey.update_attributes(data)
+      flash[:notice] = "Guardado!"
+    else
+      flash[:error] = "Horror!"
+    end
+    render :action => 'edit'
+
+  end
+
   def show
     @user = User.find params[:id]
   end
